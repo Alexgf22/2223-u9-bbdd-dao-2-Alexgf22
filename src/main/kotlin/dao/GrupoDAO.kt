3c839c01858479a2f7cdc86f0f847a3100835e92
Alexgf22 <agarcia@iesrafaelalberti.es>
2023-05-04 13:56:30 +0200

    refactorizando las funciones

--- src/main/kotlin/dao/GrupoDAO.kt
+++ src/main/kotlin/dao/GrupoDAO.kt
@@ -24,34 +24,39 @@ class GrupoDAO(private val dataSource: DataSource): IGrupoDao {
 
 
     override fun crearGrupo(grupo: Grupo) {
-        val stmt = conexion.prepareStatement("""
-            INSERT INTO GRUPOS(grupodesc) VALUES(?)
-        """.trimIndent(), Statement.RETURN_GENERATED_KEYS)
-        stmt.setString(1, grupo.grupodesc)
-        stmt.executeUpdate()
+        val sql = "INSERT INTO GRUPOS(grupodesc) VALUES(?)"
+        return dataSource.connection.use { conn ->
+            conn.prepareStatement(sql).use { stmt ->
+                stmt.setString(1, grupo.grupodesc)
+                stmt.executeUpdate()
+                grupo
 
-        val generatedKeys = stmt.generatedKeys
-        if (generatedKeys.next()) {
-            val id = generatedKeys.getInt(1)
-            grupo.grupoid = id
+                val generatedKeys = stmt.generatedKeys
+                if (generatedKeys.next()) {
+                    val id = generatedKeys.getInt(1)
+                    grupo.grupoid = id
+                }
+            }
         }
 
     }
 
     override fun obtenerGrupo(id: Int): Grupo? {
-        val stmt = conexion.prepareStatement("""
-            SELECT * FROM GRUPOS WHERE grupoid=?
-        """.trimIndent())
-        stmt.setInt(1, id)
-        val rs = stmt.executeQuery()
-        return if (rs.next()) {
-            Grupo(
-                rs.getInt("grupoid"),
-                rs.getString("grupodesc"),
-                rs.getInt("mejorposCTFid")
-            )
-        } else {
-            null
+        val sql2 = "SELECT * FROM GRUPOS WHERE grupoid=?"
+        return dataSource.connection.use { conn ->
+            conn.prepareStatement(sql2).use { stmt ->
+                stmt.setInt(1, id)
+                val rs = stmt.executeQuery()
+                if (rs.next()) {
+                    Grupo(
+                        rs.getInt("grupoid"),
+                        rs.getString("grupodesc"),
+                        rs.getInt("mejorposCTFid")
+                    )
+                } else {
+                    null
+                }
+            }
         }
 
     }
@@ -61,20 +66,25 @@ class GrupoDAO(private val dataSource: DataSource): IGrupoDao {
 
 
     override fun actualizarMejorPosCtf(grupo: Grupo) {
-        val stmt = conexion.prepareStatement("""
-            SELECT CTFid, puntuacion FROM CTFS WHERE grupoid=? ORDER BY puntuacion DESC LIMIT 1
-        """.trimIndent())
-        stmt.setInt(1, grupo.grupoid)
-        val rs = stmt.executeQuery()
-        if (rs.next()) {
-            val mejorCTFid = rs.getInt("CTFid")
-            grupo.mejorPosCTFid = mejorCTFid
-            val updateStmt = conexion.prepareStatement("""
-                UPDATE GRUPOS SET mejorposCTFid=? WHERE grupoid=?
-            """.trimIndent())
-            updateStmt.setInt(1, mejorCTFid)
-            updateStmt.setInt(2, grupo.grupoid)
-            updateStmt.executeUpdate()
+        val sql3 = "SELECT CTFid, puntuacion FROM CTFS WHERE grupoid=? ORDER BY puntuacion DESC LIMIT 1"
+        return dataSource.connection.use { conn ->
+            conn.prepareStatement(sql3).use { stmt ->
+                stmt.setInt(1, grupo.grupoid)
+                val rs = stmt.executeQuery()
+                if (rs.next()) {
+                    val mejorCTFid = rs.getInt("CTFid")
+                    grupo.mejorPosCTFid = mejorCTFid
+                    val updateStmt = conexion.prepareStatement(
+                        """
+                        UPDATE GRUPOS SET mejorposCTFid=? WHERE grupoid=?
+                    """.trimIndent()
+                    )
+                    updateStmt.setInt(1, mejorCTFid)
+                    updateStmt.setInt(2, grupo.grupoid)
+                    updateStmt.executeUpdate()
+                    grupo
+                }
+            }
         }
 
     }
@@ -83,36 +93,44 @@ class GrupoDAO(private val dataSource: DataSource): IGrupoDao {
 
 
     override fun eliminarGrupo(id: Int) {
-        val stmt = conexion.prepareStatement("""
-        DELETE FROM GRUPOS WHERE grupoid=?
-    """.trimIndent())
-        stmt.setInt(1, id)
-        stmt.executeUpdate()
+        val sql4 = "DELETE FROM GRUPOS WHERE grupoid=?"
+        return dataSource.connection.use { conn ->
+            conn.prepareStatement(sql4).use { stmt ->
+                stmt.setInt(1, id)
+                stmt.executeUpdate()
+            }
+        }
 
     }
 
     override fun obtenerTodosGrupos(): List<Grupo> {
-        val stmt = conexion.createStatement()
-        val rs = stmt.executeQuery("SELECT * FROM GRUPOS")
-        val grupos = mutableListOf<Grupo>()
-        while (rs.next()) {
-            grupos.add(
-                Grupo(
-                    rs.getInt("grupoid"),
-                    rs.getString("grupodesc"),
-                    rs.getInt("mejorposCTFid")
-                )
-            )
+        val sql5 = "SELECT * FROM GRUPOS"
+        return dataSource.connection.use { conn ->
+            conn.prepareStatement(sql5).use { stmt ->
+                val rs = stmt.executeQuery()
+                val grupos = mutableListOf<Grupo>()
+                while (rs.next()) {
+                    grupos.add(
+                        Grupo(
+                            rs.getInt("grupoid"),
+                            rs.getString("grupodesc"),
+                            rs.getInt("mejorposCTFid")
+                        )
+                    )
+                }
+                grupos
+            }
         }
-        return grupos
 
     }
 
     override fun eliminarTodosGrupos() {
-        val stmt = conexion.prepareStatement("""
-        DELETE FROM GRUPOS
-    """.trimIndent())
-        stmt.executeUpdate()
+        val sql6 = "DELETE FROM GRUPOS"
+        return dataSource.connection.use { conn ->
+            conn.prepareStatement(sql6).use { stmt ->
+                stmt.executeUpdate()
+            }
+        }
 
     }
 
